805ebb043fa2c334b9d5f53b38241a10cf8aa0de
Nonnon <sabeet57@gmail.com>
2026-04-09 03:37:47 +0700

    Change secret file

--- include/uno/Debug.h
+++ include/uno/Debug.h
@@ -7,7 +7,7 @@
 // ตั้งค่าเป็น 1 เมื่อต่อคอมพิวเตอร์เพื่อดูค่า
 // ตั้งค่าเป็น 0 เมื่อนำไปต่อแผงใช้งานจริง 
 // ==========================================
-#define ENABLE_DEBUG 0
+#define ENABLE_DEBUG 1
 #if ENABLE_DEBUG
   #define DEBUG_BEGIN(baud) Serial.begin(baud)
   #define DEBUG_PRINT(x)    Serial.print(x)
